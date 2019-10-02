a7b1c87f842427e65c9857d1c0f1727dbc52d37c
houstonmtz <54007858+houstonmtz@users.noreply.github.com>
2019-10-01 21:45:30 -0500

    Calculating the sum and products of user inputs.
    Program is done with while loops and for loops for experimentation on efficiency.

---
+++ Lab5b_b.py
@@ -0,0 +1,65 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Houston Martinez
+# Section:      526
+# Assignment:   Lab 5b-b
+# Date:         26 September 2019
+#
+
+print("This program will calculate the sum of all the integers from 0 to the number entered from the user using a for"
+      " loop.")
+integer = int(input("Please enter the integer to calculate the sum of all integers before it to 0: "))
+totalvalue = 0
+numbersbelow = 1
+integer = integer + 1
+for numbersbelow in range(integer):
+    totalvalue += numbersbelow
+
+print(totalvalue)
+print("-------------------------------------------------------------------------------------------------------")
+
+print("This program will calculate the sum of all the integers from 0 to the number entered from the user using a while"
+      " loop.")
+integer = int(input("Please enter the integer to calculate the sum of all integers before it to 0: "))
+
+if integer > 0:
+    integerbelow = integer - 1
+    totalvalue = integer + integerbelow
+    while integerbelow >= 0:
+        integerbelow = integerbelow - 1
+        totalvalue += integerbelow
+        if integerbelow <= 0:
+            break
+else:
+    print("Error! First value entered was equal to or below 0.")
+print(totalvalue)
+print("-------------------------------------------------------------------------------------------------------")
+
+print("This program will calculate the product of all the integers from 1 to the number entered from the user using a "
+      "for loop.")
+integer = int(input("Please enter the integer to calculate the product of all integers before it to 1: "))
+totalvalue = 1
+numbersbelow = 1
+integer += 1
+for numbersbelow in range(1, integer):
+    totalvalue *= numbersbelow
+
+print(totalvalue)
+print("-------------------------------------------------------------------------------------------------------")
+
+print("This program will calculate the product of all the integers from 1 to the number entered from the user using a"
+      " while loop.")
+integer = int(input("Please enter the integer to calculate the product of all integers before it to 1: "))
+if integer > 1:
+    integerbelow = integer - 1
+    totalvalue = integer * integerbelow
+    while integerbelow >= 1:
+        integerbelow = integerbelow - 1
+        totalvalue *= integerbelow
+        if integerbelow <= 1:
+            break
+
+print(totalvalue)
+print("-------------------------------------------------------------------------------------------------------")
